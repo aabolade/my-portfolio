0cc1a18d21e526b04446eba5ac61748e15d07aa4
Leke Abolade <adeleke.abolade@theappbusiness.com>
2020-04-12 15:00:06 +0100

    Updates

---
+++ precache-manifest.8cfb7ff0cb79bb7b631046caa3401375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044ac9e020996f7d5188157acd98bfe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729e03d583ff60c20f54",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "efac15ddfb766fd83234",
+    "url": "/static/css/main.f7c9c040.chunk.css"
+  },
+  {
+    "revision": "729e03d583ff60c20f54",
+    "url": "/static/js/2.fd20754e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.fd20754e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efac15ddfb766fd83234",
+    "url": "/static/js/main.3d52f3f1.chunk.js"
+  },
+  {
+    "revision": "5fad53acd982f5e46321",
+    "url": "/static/js/runtime-main.b6d90ca0.js"
+  }
+]);
